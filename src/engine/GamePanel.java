241a50fdb6c735949434e425709f0a937b485fe6
Frone <pedro.frone@gmail.com>
2026-04-01 22:44:36 -0300

    edit Mapa

--- src/engine/GamePanel.java
+++ src/engine/GamePanel.java
@@ -17,7 +17,7 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenHeight = tileSize * maxScreenRow;
     
     public final int maxWorldCol = 50; 
-    public final int maxWorldRow = 12; 
+    public final int maxWorldRow = 14; 
     public final int worldWidth = tileSize * maxWorldCol;
     public final int worldHeight = tileSize * maxWorldRow;
 
